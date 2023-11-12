65e53c1af73f0e1eda5316fedfffd81fcb85a0da
ChoIkJun <89628722+ikkjun@users.noreply.github.com>
2023-11-12 12:29:11 +0900

    Add files via upload

--- ch14/Exercise/Exercise14_5.java
+++ ch14/Exercise/Exercise14_5.java
@@ -1,13 +1,16 @@
+import java.util.Comparator;
+import java.util.stream.Stream;
 
 public class Exercise14_5 {
 
 	public static void main(String[] args) {
 		int sum = 0;
 		String[] strArr = {"aaa","bb","c","dddd"};
-		for(int i=0;i<strArr.length;i++) {
-			sum += strArr[i].length();
-		}
-		System.out.println("sum=" + sum);
+		Stream<String> strStream = Stream.of(strArr);
+		
+		strStream.map(String::length)	// strStream.map(s -> s.length())
+			.sorted(Comparator.reverseOrder())		// 문자열 길이로 역순 정렬
+			.limit(1).forEach(System.out::println);	// 제일 긴 문자열의 길이 출력
 	}
 
 }
