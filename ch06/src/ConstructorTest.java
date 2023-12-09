69448f67b6f46b0a68a242ae172c34630cdeaef1
ikkjun <89628722+ikkjun@users.noreply.github.com>
2023-12-09 09:53:07 +0900

    java_jungsuk

---
+++ ch06/src/ConstructorTest.java
@@ -0,0 +1,19 @@
+class Data1{
+	int value;
+}
+
+class Data2{
+	int value;
+//	Data2(){} // 기본 생성자 추가하면 오류 안 뜬다.
+	Data2(int x){	// 매개변수가 있는 생성자.
+		value = x;
+	}
+}
+public class ConstructorTest {
+
+	public static void main(String[] args) {
+		Data1 d1 = new Data1();
+		Data2 d2 = new Data2();	// compile error 발생
+	}
+
+}
