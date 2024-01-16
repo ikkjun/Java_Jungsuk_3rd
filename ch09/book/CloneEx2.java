c6f0c7f14e1bff0d2900100ce223c99f7f7fd5e3
ikkjun <89628722+ikkjun@users.noreply.github.com>
2024-01-16 19:39:52 +0900

    java study

--- ch09/book/CloneEx2.java
+++ ch09/book/CloneEx2.java
@@ -1,33 +1,16 @@
 package Examples;
 
-class Point implements Cloneable {
-	int x, y;
-	
-	Point(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-	
-	public String toString() {
-		return "x=" + x + ", y=" + y;
-	}
-	
-	public Object clone() {
-		Object obj = null;
-		try {
-			obj = super.clone();	// clone() 은 반드시 예외처리를 해주어야 한다.
-		} catch(CloneNotSupportedException e) {}
-		return obj;
-	}
-}
+import java.util.Arrays;
 
-public class CloneEx2 {
+public class CloneEx1 {
 
 	public static void main(String[] args) {
-		PointCopy original = new PointCopy(3,5);
-		PointCopy copy = (PointCopy)original.clone();	// 복제(clone)해서 새로운 객체 생성
-		System.out.println(original);
-		System.out.println(copy);
+		int[] arr = {1,2,3,4,5};
+		int[] arrClone = arr.clone();	// 배열 arr을 복제해서 같은 내용의 새로운 배열을 만든다.
+		arrClone[0] = 6;
+
+		System.out.println(Arrays.toString(arr));
+		System.out.println(Arrays.toString(arrClone));
 	}
 
 }
