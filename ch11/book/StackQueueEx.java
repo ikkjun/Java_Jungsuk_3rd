48887777c8db4207ad0782cde5f391064d34c321
ikkjun <89628722+ikkjun@users.noreply.github.com>
2024-01-24 12:05:59 +0900

    java study

--- ch11/book/StackQueueEx.java
+++ ch11/book/StackQueueEx.java
@@ -4,6 +4,15 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 
+/**
+ * stack타입의 참조변수 st를 선언하고 stack타입의 새로운 객체 생성
+ * queue타입의 참조변수 q를 선언하고 구현체로 LinkedList를 사용하시오.
+ *
+ * st에 "0", "1", "2"를 저장하시오.
+ * q에 "0", "1", "2"를 저장
+ *
+ * st와 q가 비어있지 않을 때까지 하나씩 출력하시오.
+ */
 public class StackQueueEx {
 
 	public static void main(String[] args) {
